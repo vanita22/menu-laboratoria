b6a84ada5f60d58e8606664350fcf4f7c6ced1fa
vanita22 <vanitayrafita2004@gmail.com>
2017-06-07 01:15:29 -0300

    subiendo nuevos cambios

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	var header = document.getElementById('header');
+	var headroom = new Headroom(header);
+	headroom.init();/*nos muestra en que posición del body están las pestañas en el body*/
+
+});
+
+alert("hola mundoi");
